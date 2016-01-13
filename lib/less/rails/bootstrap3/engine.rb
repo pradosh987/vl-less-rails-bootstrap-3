15edd081d1617052aed250b8434b081bad5c9f39
Pradosh Gaonkar <pradosh987@gmail.com>
2016-01-13 16:05:40 +0530

    minor fix

--- lib/less/rails/bootstrap3/engine.rb
+++ lib/less/rails/bootstrap3/engine.rb
@@ -5,7 +5,7 @@ module Less
     module Bootstrap3
       class Engine < ::Rails::Engine
         
-        initializer 'less-rails-bootstrap.setup', :after => 'less-rails.before.load_config_initializers', :group => :all do |app|
+        initializer 'vl_less-rails-bootstrap_3.setup', :after => 'less-rails.before.load_config_initializers', :group => :all do |app|
           app.config.less.paths << File.join(config.root, 'app', 'frameworks')
         end
         
